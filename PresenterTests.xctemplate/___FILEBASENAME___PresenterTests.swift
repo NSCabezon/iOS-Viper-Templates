de6e05a6b584ba3bd39e38cfbd083536ef28f2c0
NSCabezon <nscabezon@gmail.com>
2020-03-29 14:01:42 +0200

    renamed variables

--- PresenterTests.xctemplate/___FILEBASENAME___PresenterTests.swift
+++ PresenterTests.xctemplate/___FILEBASENAME___PresenterTests.swift
@@ -4,17 +4,17 @@ import XCTest
 
 class ___VARIABLE_productName:identifier___PresenterTests: XCTestCase {
 	var presenter: ___VARIABLE_productName:identifier___Presenter!
-	var mockView: ___VARIABLE_productName:identifier___ViewControllerMock!
-	var mockInteractor: ___VARIABLE_productName:identifier___InteractorMock!
-	var mockRouter: ___VARIABLE_productName:identifier___RouterMock!
+	var viewMock: ___VARIABLE_productName:identifier___ViewControllerMock!
+	var interactorMock: ___VARIABLE_productName:identifier___InteractorMock!
+	var routerMock: ___VARIABLE_productName:identifier___RouterMock!
 	
 	override func setUp() {
-		mockView = ___VARIABLE_productName:identifier___ViewControllerMock()
-		mockInteractor = ___VARIABLE_productName:identifier___InteractorMock()
-		mockRouter = ___VARIABLE_productName:identifier___RouterMock()
-		presenter = ___VARIABLE_productName:identifier___Presenter(interface: mockView, interactor: mockInteractor, router: mockRouter)
+		viewMock = ___VARIABLE_productName:identifier___ViewControllerMock()
+		interactorMock = ___VARIABLE_productName:identifier___InteractorMock()
+		routerMock = ___VARIABLE_productName:identifier___RouterMock()
+		presenter = ___VARIABLE_productName:identifier___Presenter(interface: viewMock, interactor: interactorMock, router: routerMock)
 	}
-
+	
 	override func tearDown() {
 		// Put teardown code here. This method is called after the invocation of each test method in the class.
 	}
